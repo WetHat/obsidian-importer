30bbb04ccce60466cbd9e7193ff72a48d9a68b20
WetHat <PeterErnst@outlook.de>
2024-09-24 21:46:23 +0200

    Moved BookMetadata to file of EpubBook and made private

--- src/formats/ebooks/epub/epub-import.ts
+++ src/formats/ebooks/epub/epub-import.ts
@@ -3,8 +3,104 @@ import { parseFilePath, PickedFile } from "filesystem";
 import { ImportContext } from "main";
 import { Vault, TFolder } from "obsidian";
 import { readZip, ZipEntryFile } from "zip";
-import { ImportableAsset, BookMetadata, MediaAsset, PageAsset, TocAsset } from "./epub-assets";
-import { titleToBasename } from "../ebook-transformers";
+import { ImportableAsset, MediaAsset, PageAsset, TocAsset } from "./epub-assets";
+import { frontmatterTagname, titleToBasename } from "../ebook-transformers";
+
+/**
+ * A utility class to parse meta information of the book as specified in the
+ * `opf` file
+ *
+ * THe relevant section in that file has this form
+ *
+ * ~~~xml
+ * <metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">
+ *     <dc:title>C# 8.0 in a Nutshell: The Definitive Reference</dc:title>
+ *     ...
+ * </metadata>
+ * ~~~
+ *
+ * ❗The namespace of the property names is removed.
+ */
+class BookMetadata {
+    private meta = new Map<string, string[]>();
+
+    /**
+     * Build a new instance by parsing the `<metadata>` section of the book's
+     * content file.
+     *
+     * @param pkg The `<package>` element (root of the content file).
+     */
+    constructor(pkg: Element) {
+        const metadata = pkg.querySelector('package > metadata');
+        if (metadata) {
+            const
+                c = metadata.children,
+                cCount = c.length;
+            for (let i = 0; i < cCount; i++) {
+                // extract the metadata
+                const
+                    node = c[i],
+                    nodeName = node.nodeName;
+                let
+                    key: string | null,
+                    value: string | null;
+
+                if (nodeName === "meta") {
+                    key = node.getAttribute("name");
+                    value = node.getAttribute("content");
+                } else {
+                    key = nodeName;
+                    value = node.textContent;
+                    if (key) {
+                        const colonIndex = key.indexOf(':');
+                        key = colonIndex >= 0 ? key.slice(colonIndex + 1) : key;
+                    }
+                }
+
+                if (key && value) {
+                    this.setProperty(key, value); // capture the metadata
+                }
+            }
+        }
+        // make sure we have the cover page
+        if (!this.meta.has("coverPage")) {
+            // get the cover image gtom the cover page then
+            const coverPage = pkg.querySelector('package > guide > reference[type="cover"]');
+            if (coverPage) {
+                const href = coverPage.getAttribute("href");
+                this.meta.set("coverPage", href ? [href] : []);
+            }
+        }
+    }
+
+    private setProperty(name: string, value: string) {
+        const entry = this.meta.get(name);
+        if (entry) {
+            entry.push(value);
+        } else {
+            this.meta.set(name, [value]);
+        }
+    }
+
+    /**
+     * Get a property value as a string.
+     * @param name Property name (without namespace).
+     * @returns property value (as a comma separated list if there is more than one value
+     *          for that property).
+     */
+    asString(name: string): string | undefined {
+        return this.meta.get(name)?.join(",");
+    }
+
+    /**
+     * Get the property value(s) as array.
+     * @param name Property name (without namespace).
+     * @returns Array of property values
+     */
+    asArray(name: string): string[] | undefined {
+        return this.meta.get(name);
+    }
+}
 
 /**
  * Representation of an e-pub book prepared for imported to Obsidian.
@@ -18,15 +114,31 @@ export class EpubBook {
     private mimeMap = new Map<string, string>(); // asset source path => mimetype
     private assetMap = new Map<string, ImportableAsset>(); // asset source path => book asset
     readonly parser = new DOMParser(); // the parser instance to use
-    bookMeta: BookMetadata; // The books metadata
+
+    private meta: BookMetadata; // The books metadata
+
+    coverImage?: string;
+    tags: string[] = [];
 
     // some progress data
     ctx: ImportContext;
     fileCount = 0;
     processed = 0;
 
-    get bookTitle(): string {
-        return this.bookMeta.asString("title") ?? 'Untitled Book';
+    get title(): string {
+        return this.meta.asString("title") ?? 'Untitled book';
+    }
+
+    get author(): string {
+        return this.meta.asString("creator") ?? "Unkown author";
+    }
+
+    get publisher(): string {
+        return this.meta.asString("publisher") ?? "Unknown publisher";
+    }
+
+    get description(): string | undefined {
+        return this.meta.asString("description") ?? undefined;
     }
 
     constructor(vault: Vault, ctx: ImportContext) {
@@ -62,7 +174,9 @@ export class EpubBook {
         }
 
         // extract the book meta information;
-        this.bookMeta = new BookMetadata(root);
+        this.meta = new BookMetadata(root);
+        this.tags = this.meta.asArray("subject") ?? ["e-book"];
+        this.tags = this.tags.map(t => frontmatterTagname(t));
     }
 
     /**
@@ -118,8 +232,13 @@ export class EpubBook {
                         this.assetMap.set(href, this.toc);
                         break;
                     case 'text/css':
-                    case '?':
                         // we are going to ignore stylesheets and files not in the manifest
+                        this.ctx.reportProgress(++this.processed, this.fileCount);
+                        break;
+                    case '?':
+                        if ("opf" !== source.extension) {
+                            this.ctx.reportSkipped(`'${source.name}' is not part of the book`);
+                        }
                         this.ctx.reportProgress(++this.processed, this.fileCount);
                         break;
                     default:
@@ -127,16 +246,38 @@ export class EpubBook {
                             // media can me imported without pre-processing
                             this.assetMap.set(href, new MediaAsset(source, href, mimetype));
                         } else {
-                            this.ctx.reportSkipped(`"${source.name}" has unsupported mimetype: ${mimetype}`);
+                            this.ctx.reportSkipped(`'${source.name}' has unsupported mimetype: ${mimetype}`);
                             this.ctx.reportProgress(++this.processed, this.fileCount);
                         }
                         break;
                 }
             } else {
-                this.ctx.reportSkipped(`"${source.name}" is not part of the book`);
+                this.ctx.reportSkipped(`'${source.name}' is not part of the book`);
                 this.ctx.reportProgress(++this.processed, this.fileCount);
             }
         }
+        // now, that we have all relevant assets, we can augment the metadata
+        this.coverImage = this.meta.asString("cover");
+        if (!this.coverImage) {
+            // get it from the cover page then
+            const coverPage = this.meta.asString("coverPage");
+            if (coverPage) {
+                const asset = this.getAsset(coverPage);
+                // find the image in the content
+                if (asset instanceof PageAsset && asset.page) {
+                    const images = asset.page.body.getElementsByTagName("img");
+                    if (images.length > 0) {
+                        const src = images[0].getAttribute("src");
+                        if (src) {
+                            this.coverImage = src.replace(/\.\.\//g, ""); // make relative to top
+                        }
+                    }
+                }
+            }
+        }
+
+        // ... and complete initialization to the content map
+        await this.toc.parse(this);
     }
 
     async import(outputFolder: TFolder): Promise<void> {
@@ -147,9 +288,6 @@ export class EpubBook {
         }
         const bookFolder = await this.vault.createFolder(bookFolderPath);
         this.ctx.status(`Saving Ebook to ${bookFolder.path}`);
-
-        // prepare the assets for import
-        await this.toc.parse(this);
 
         // reconnect the link targets in all pages
         for (const asset of this.assetMap.values()) {
